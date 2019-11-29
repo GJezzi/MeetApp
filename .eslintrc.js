790199fb3e241603574faf6005aa3a95793623a4
GJezzi <gjezzi@hotmail.com>
2019-11-29 14:18:09 -0300

    First Commit

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+    env: {
+        es6: true,
+        jest: true,
+        browser: true,
+    },
+    extends: ['airbnb', 'prettier', 'prettier/react'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        __DEV__: true,
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier', 'import', 'react-hooks', 'jsx-a11y'],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'error',
+            {
+                extensions: ['.jsx', '.js'],
+            },
+        ],
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        'react/jsx-one-expression-per-line': 'off',
+        'global-require': 'off',
+        'react-native/no-raw-text': 'off',
+        'no-param-reassign': 'off',
+        'no-underscore-dangle': 'off',
+        camelcase: 'off',
+        'no-console': ['error', { allow: ['tron'] }],
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/jsx-props-no-spreading': 'off',
+        'react/prop-types': ['error', { ignore: ['navigation'] }],
+        'prefer-object-spread': 'off',
+        'no-extraneous-dependencies': 'off',
+        'import-no-unresolved': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            'babel-plugin-root-import': {
+                rootPathSuffix: 'src',
+            },
+        },
+    },
+};
